ba00e2c639dbb29aa6c517daa5e13da1d6d222d1
Neely <neely@Neelys-MBP.HPH.Local>
2021-04-22 11:22:52 -0500

    finish Dougie api

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
-const baseUrl =
-	'https://data.cityofnewyork.us/resource/erm2-nwe9.json?borough=BRONX';
+function getData(){
+    let borough = event.target.id;
+    let limit = document.getElementById("userInput").value
+    if (limit ==""){
+limit= 10
+    }
+let api = "https://data.cityofnewyork.us/resource/erm2-nwe9.json?"
++ "agency=NYPD"
++ "&borough="
++ borough 
++"&$limit="
++limit
 
-// When the user clicks on one of the five buttons, a list of complaints should be displayed on the page, according to the number they input AND the borough they clicked on
+fetch(api) 
+.then((data) =>{
+    return data.json();
 
- 
-const Bronx = document.querySelector('input[value=BRONX]');
-const form = document.querySelector('form');
-const display = document.getElementById('display')
+    }).then((formattedData) =>{
 
-form.addEventListener('submit', (event) => {
-	event.preventDefault();
-	fetch(baseUrl)
-		// userInput.value
-		.then((res) => res.json())
-		.then((resJson) => {
-			console.log(resJson);
-			display.innerText = resJson[0].complaint_type;
-		});
-})
+    displayData(formattedData)
+
+});
+}
+
+document.getElementById('BROOKLYN').addEventListener('click', getData);
+document.getElementById('MANHATTAN').addEventListener('click', getData);
+document.getElementById('QUEENS').addEventListener('click', getData);
+document.getElementById('STATEN ISLAND').addEventListener('click', getData);
+document.getElementById('BRONX').addEventListener('click', getData);
+
+function displayData(data) {
+	document.getElementById('div1').innerHTML = '';
+
+	for (let i = 0; i < data.length; i++) {
+		document.getElementById('div1').innerHTML +=
+			'<p>' + data[i].descriptor + '</p>';
+		document.getElementById('div1').innerHTML +=
+			'<details> <summary> POLICE RESPONSE </summary>' +
+			data[i].resolution_description +
+			'</details>';
+	}
+}
